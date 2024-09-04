945afad161541c823917ac38bb7050f5264d3998
tanulo@example.com <tanulo@example.com>
2024-09-04 10:06:25 +0200

    Guess

--- index.ts
+++ index.ts
@@ -1,2 +1,23 @@
-let greet: string = "Hello Typescript";
-console.log(greet);
+const numEl = document.getElementById("inputNum") as HTMLInputElement;
+const resultEl = document.getElementById("result") as HTMLParagraphElement;
+
+const theNumber = Math.floor(Math.random() * 100) + 1;
+
+function guess() {
+    const guessNumber: number = numEl.valueAsNumber;
+    if (isCorrect(guessNumber)) {
+        resultEl.innerText = "Eltaláltad";
+    }
+    else {
+        if (guessNumber < theNumber) {
+            resultEl.innerText = "A szám nagyobb";
+        }
+        else {
+            resultEl.innerText = "A szám kisebb";
+        }
+    }
+}
+
+function isCorrect(guessNumber: number): boolean {
+    return guessNumber === theNumber;
+}
